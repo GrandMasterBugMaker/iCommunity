6eba2e1da6dbcfbd888bd9611d2caedc32819b4f
lingnanbai <lingnanbai@gmail.com>
2021-03-11 22:11:26 +0800

    Initial commit

---
+++ src/main/java/com/bailingnan/icommunity/ICommunityApplication.java
@@ -0,0 +1,13 @@
+package com.bailingnan.icommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ICommunityApplication.class, args);
+    }
+
+}
