56988b4135ff8e87308cfa64ad0448db605b03f5
lingnanbai <lingnanbai@gmail.com>
2021-03-27 00:14:08 +0800

    update

--- src/main/java/com/bailingnan/icommunity/controller/LoginController.java
+++ src/main/java/com/bailingnan/icommunity/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.bailingnan.icommunity.controller;
 
+import com.bailingnan.icommunity.entity.User;
+import com.bailingnan.icommunity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 /**
  * @author bailingnan
@@ -10,8 +16,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path="/register",method = RequestMethod.GET)
     public String getRegisterPage() {
         return "/site/register";
+    }
+
+    @RequestMapping(path="/register",method=RequestMethod.POST)
+    public String register(Model model, User user){
+        Map<String,Object> map = userService.register(user);
+        if(map==null||map.isEmpty()){
+            model.addAttribute("msg","注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
+            model.addAttribute("target","/index");
+            return "/site/operate-result";
+        }else{
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            model.addAttribute("emailMsg",map.get("emailMsg"));
+            return "/site/register";
+        }
     }
 }
