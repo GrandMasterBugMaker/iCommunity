5f9f90fbe3ca5f1ce8017a393027a4c9fca12503
lingnanbai <lingnanbai@gmail.com>
2021-03-30 20:58:26 +0800

    update

--- src/main/java/com/bailingnan/icommunity/service/DiscussPostService.java
+++ src/main/java/com/bailingnan/icommunity/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.bailingnan.icommunity.service;
 
 import com.bailingnan.icommunity.dao.DiscussPostMapper;
 import com.bailingnan.icommunity.entity.DiscussPost;
+import com.bailingnan.icommunity.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -15,10 +17,30 @@ import java.util.List;
 public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
-    public List<DiscussPost> findDiscussPosts(int userId,int offset,int limit){
-        return discussPostMapper.selectDiscussPosts(userId,offset,limit);
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
+        return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
-    public int findDiscussPostRows(int userId){
+
+    public int findDiscussPostRows(int userId) {
+
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+    public int addDiscussPost(DiscussPost post) {
+        if (post == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+
+        // 转义HTML标记
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
     }
 }
